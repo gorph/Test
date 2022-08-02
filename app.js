d4fe950c990524e8a1937079a8a4340a5eb32745
gorph <carrionk@hotmail.com>
2022-08-02 02:00:42 -0500

    Change in Code adding js file

---
+++ app.js
@@ -0,0 +1,14 @@
+const name = "John";
+const age=39;
+const job='Web Developer';
+let myHtml;
+
+myHtml=`
+<ul>
+   <li>Name: ${name}</li>
+   <li>Age: ${age}</li>
+   <li>Job: ${job}</li>
+</ul>
+`;
+
+document.body.innerHTML=myHtml;

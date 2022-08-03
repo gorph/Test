7b7614d9000cf86d7a3d2903c80c12606033bd3b
gorph <carrionk@hotmail.com>
2022-08-02 23:40:11 -0500

    JS change Template Literals

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const age=39;
 const job='Web Developer';
 let myHtml;
 
+function hello(){
+   return 'hello 123';
+}
+
 myHtml=`
 <ul>
    <li>Name: ${name}</li>
    <li id='age'>Age: ${age}</li>
    <li>Job: ${job}</li>
+   <li>${age>30 ? 'Over 30':'Under 30'}</li>
+   <li>Msg: ${hello()}</li>
 </ul>
 `;
 

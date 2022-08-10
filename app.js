dd2a0821c5c31c4c01f40b3ea1f74c05a464c806
gorph <carrionk@hotmail.com>
2022-08-10 00:20:26 -0500

    change

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ myHtml=`
    <li>${age>30 ? 'Over 30':'Under 30'}</li>
    <li>Msg: ${hello()}</li>
 </ul>
-<p div='obj_lit'>Insert Other Messages Here</p>
+//<p div='obj_lit'>Insert Other Messages Here</p>
 `;
 
 document.getElementById('ulist').innerHTML+=myHtml;
